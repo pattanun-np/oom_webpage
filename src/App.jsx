205a1bafd4429467f217454d6792c0bf2bd68e26
PattanunNP <phattanun19@hotmail.com>
2019-01-31 21:03:27 +0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
 import { BrowserRouter as Router, Route , Switch} from 'react-router-dom'
 
 class App extends Component {
