28dc43c3feb97542c65b7c636e5c4867478a1e11
PattanunNP <phattanun19@hotmail.com>
2019-01-31 21:09:03 +0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route , Switch} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import News from './components/News';
+import Gallery from './components/Gallery';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
+    <div>
       <Switch>
                 <Route exact path="/" component={ Home } />
                 <Route  exact path="/news" component={News} />
                 <Route  exact path="/gallery" component={Gallery} />
                 <Route  exact path="/about" component={About} />
       </Switch>
-
-  
-       </div>
+   </div>
 
     );
   }
